175daee111fca4b512760764bff22c41829d9683
TimsTomatos <imshiny808@gmail.com>
2017-11-27 11:12:43 -1000

    Done

--- alien.js
+++ alien.js
@@ -1,27 +1,26 @@
 var green = 4000;
 var red = 5000;
-var blue = 2000;
+var blue = 1000;
 
 function aliens(x,y,z) {
 	if ((x + y + z) == 10000) {
-		if (x >= 5000) {
-		return "Mother of God, not the green ones";
+		if (x >= 4000) {
+			return "Mother of God, not the green ones";
 		}
-		else if (y + x === 8000) {
+		if (y + x === 3000) {
 			return "DOOOOOM!";
 		}	
-		 else if (x + z <= 3000) {
+		if (x + z <= 3000) {
 		 	return "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY";
 		 }
-		 else {
+		else {
 		 	return "We will fight!";
 		 }
-	    else {
-		return "We screwed"
+		
+	}else { 
+		return "We screwed";	 
 	}
-		 
-
-	}
+	
 
 }
 var output = aliens(green, red, blue);
